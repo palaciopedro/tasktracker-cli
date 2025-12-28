0faab158c97a5e2e4537021d37774c70d483050e
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2025-12-28 01:27:19 -0300

    basic functions created

--- tasktracker.py
+++ tasktracker.py
@@ -1,2 +1,28 @@
-from json import *
+tasks = []
 
+def add(description):
+    task = {'id': len(tasks) + 1, 'description': description, 'status': 'in progress'}
+
+    tasks.append(task)
+
+
+def update(id, new_description):
+    tasks[id - 1]['description'] = new_description
+
+
+def delete(id):
+    tasks.pop(id - 1)
+    for i, e in enumerate(tasks):
+        e['id'] = i + 1
+
+
+def list():
+    print(tasks)
+
+add('lavar louça')
+add('cozinhar macarrao')
+add('estudar')
+update(1, 'lavar e secar louça')
+delete(2)
+
+list()
