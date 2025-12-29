89e10b89670df3f089a61e45620abd9fbdc04c52
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2025-12-29 11:38:03 -0300

    add list functions

--- tasktracker.py
+++ tasktracker.py
@@ -1,28 +1,73 @@
 tasks = []
 
 def add(description):
-    task = {'id': len(tasks) + 1, 'description': description, 'status': 'in progress'}
+    task = {
+        'id': len(tasks) + 1, 
+        'description': description, 
+        'status': 'to do'
+    }
 
     tasks.append(task)
 
 
 def update(id, new_description):
-    tasks[id - 1]['description'] = new_description
+    for t in tasks:
+        if t['id'] == id:
+            t['description'] = new_description
 
 
 def delete(id):
-    tasks.pop(id - 1)
-    for i, e in enumerate(tasks):
-        e['id'] = i + 1
+    for t in tasks:
+        if t['id'] == id:
+            tasks.remove(t)
+
+
+def mark(status, id):
+    for t in tasks:
+        if t['id'] == id:
+            t['status'] = status
 
 
 def list():
-    print(tasks)
+    for t in tasks:
+        for i in t:
+            print(f'{i}: {t[i]}')
+        print('-' * 35)
+
+
+def list_done():
+    for t in tasks:
+        if t['status'] == 'done':
+            for i in t:
+                print(f'{i}: {t[i]}')
+            print('-' * 35)
+
+
+def list_notdone():
+    for t in tasks:
+        if t['status'] != 'done':
+            for i in t:
+                print(f'{i}: {t[i]}')
+            print('-' * 35)
+
+
+def list_inprogress():
+    for t in tasks:
+        if t['status'] == 'in progress':
+            for i in t:
+                print(f'{i}: {t[i]}')
+            print('-' * 35)
+
 
 add('lavar louça')
 add('cozinhar macarrao')
 add('estudar')
 update(1, 'lavar e secar louça')
-delete(2)
+mark('done', 3)
+add('correr')
+mark('done', 4)
+add('corinthians')
+mark('in progress', 5)
+delete(5)
 
-list()
+list_inprogress()
